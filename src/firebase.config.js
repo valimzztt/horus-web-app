19215e312938cbc1e225af81f41cd6b4ec009f02
valimzztt@gmail.com <you@example.com>
2023-03-27 00:48:08 -0700

    Added firebase configuration and sign in

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDOD9PsR7-8Xv1-hRKjqM3q9vq5F36LjMc",
+  authDomain: "horus-web-app-38be7.firebaseapp.com",
+  projectId: "horus-web-app-38be7",
+  storageBucket: "horus-web-app-38be7.appspot.com",
+  messagingSenderId: "1038598188912",
+  appId: "1:1038598188912:web:c7fc67d02b2be677c9e9b5",
+  measurementId: "G-6BZ7HNNGFH"
+};
+
+export default firebaseConfig;
